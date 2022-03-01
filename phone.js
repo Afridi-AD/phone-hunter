acee9979aa1a0932ce63ab9e628a804c81db6391
Afridi <thasinafridi8924@gmail.com>
2022-03-01 20:23:09 +0600

    data load from api

--- phone.js
+++ phone.js
@@ -1,7 +1,9 @@
+
+
 const SearchPhone = ()=>{
     const searchInput = document.getElementById('search-input');
     const searchText = searchInput.value;
-    console.log(searchText);
+    // console.log(searchText);
     searchInput.value = '';
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}
     `;
@@ -16,21 +18,55 @@ const SearchPhone = ()=>{
 const displaySearchResult = data => {
     const searchResult = document.getElementById('search-result');
     data.forEach(phone => {
-        console.log(phone);
+        // console.log(phone);
 
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
         <div class="card">
-            <img src="${phone.image}" class="card-img-top" alt="...">
+            <img src="${phone.image}" class="card-img-top w-50 " alt="...">
             <div class="card-body">
-              <h5 class="card-title">${phone.phone_name}</h5>
+            <h4>${phone.brand}</h4>  
+            <h5 class="card-title">${phone.phone_name}</h5>
               <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <button onclick = 'LoadphoneDetails("${phone.slug}")' id="details-button" class ="rounded-pill px-3 bg-success">Details</button>
         </div>
+       
       </div>
         
         `;
         searchResult.appendChild(div);
 
     })
+}
+
+
+const LoadphoneDetails = phoneId =>{
+    console.log(phoneId);
+    const url = `
+    https://openapi.programming-hero.com/api/phone/${phoneId}
+    `;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayMealDetail(data));
+    
+
+}
+
+
+const displayMealDetail = phone => {
+   console.log(phone);
+   const phoneDetails = document.getElementById('phone-details');
+   const div = document.createElement('div')
+   div.classList.add('card');
+   div.innerHTML =`
+   
+   <img src="${phone.data.image}" class="card-img-top w-50" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${phone.phone_name}</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+             
+   `
+
+   phoneDetails.appendChild(div);
 }
