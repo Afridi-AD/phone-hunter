378aea25e3e0474d63fb89002d3d01b6e0908434
Afridi <thasinafridi8924@gmail.com>
2022-03-02 12:05:08 +0600

    phone error cheak

--- phone.js
+++ phone.js
@@ -5,20 +5,27 @@ const SearchPhone = ()=>{
     const searchText = searchInput.value;
     // console.log(searchText);
     searchInput.value = '';
-    const errorMessage = document.getElementById('error-message').style.display = 'none';
-    if(searchText == ''){
-       errorMessage.style.display = 'block';
-    }
-    else{
-        const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}
-        `;
-        // console.log(url);
-        fetch(url)
-        .then(res => res.json())
-        .then(data => displaySearchResult(data.data));
-        
     
+    if(searchText == ''){
+        const emptyString = document.getElementById('empty-string-error-message');
+       emptyString.style.display = 'block';
     }
+   else{
+    
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}
+    `;
+    // console.log(url);
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displaySearchResult(data.data));
+    
+ 
+    
+   }
+    
+   
+    
+    
     }
    
 
@@ -30,16 +37,17 @@ const displaySearchResult = data => {
     searchResult.textContent = '';
     info.forEach(phone => {
         // console.log(phone);
+       
 
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML = `
+    const div = document.createElement('div');
+    div.classList.add('col');
+    div.innerHTML = `
         <div class="card">
             <img src="${phone.image}" class="card-img-top w-50 " alt="...">
             <div class="card-body">
             <h4>${phone.brand}</h4>  
             <h5 class="card-title">${phone.phone_name}</h5>
-              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              
               <button onclick = 'LoadphoneDetails("${phone.slug}")' id="details-button" class ="rounded-pill px-3 bg-success">Details</button>
         </div>
        
